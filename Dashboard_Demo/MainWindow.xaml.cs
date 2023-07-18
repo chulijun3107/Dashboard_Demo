c938c0d78163b73974887137e2462ca76c10ecfa
chulijun3107 <chulijun3107@163.com>
2023-07-18 16:02:09 +0800

    first commit

---
+++ Dashboard_Demo/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dashboard_Demo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            dashboard1.Value = slider1.Value;
+            if (slider1.Value > 800000d)
+            {
+                dashboard1.ContentTemplate = FindResource("Large") as DataTemplate;
+            }
+            else
+            {
+                dashboard1.ContentTemplate = FindResource("Small") as DataTemplate;
+            }
+        }
+    }
+}
